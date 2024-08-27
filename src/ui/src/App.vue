e5baf78adf7eff994a0d3ad446b1e313c186f2ac
michalb01 <91828250+michalb01@users.noreply.github.com>
2024-08-28 01:17:46 +0200

    form

--- src/ui/src/App.vue
+++ src/ui/src/App.vue
@@ -1,31 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ClientForm />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ClientForm from './components/ClientForm.vue' // Import the new component
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ClientForm // Register the new component
   },
-  async mounted(){
-    
+  async mounted() {
     const data = await window.ghl.getUserData();
-    console.log("user-details", data)
+    console.log("user-details", data);
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+/* Optional CSS to style the form */
+form {
+  max-width: 400px;
+  margin: auto;
+}
+
+div {
+  margin-bottom: 15px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input {
+  width: 100%;
+  padding: 8px;
+  box-sizing: border-box;
+}
+
+button {
+  padding: 10px 15px;
+  background-color: #007BFF;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
 }
 </style>
