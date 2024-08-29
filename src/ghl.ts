aabf3d1c3b1c69094b7f0f3884b2d75263b1e50c
Kamil Butryn <kaybenot@gmail.com>
2024-08-30 00:36:33 +0200

    spermamniezalewa

--- src/ghl.ts
+++ src/ghl.ts
@@ -165,7 +165,7 @@ export class GHL {
         {
           "name": "PayU",
           "description": "Operator płatności internetowych, działający jako system, który daje możliwość dokonywania oraz otrzymywania wpłat przez Internet",
-          "paymentsUrl": "https://payu-9gvx.onrender.com/payment",
+          "paymentsUrl": "https://p24.onrender.com/payment",
           "queryUrl": "https://payu-9gvx.onrender.com/query",
           "imageUrl": "https://msgsndr-private.storage.googleapis.com/marketplace/apps/66cb484efa377f800409bd8e/3425444f-a209-4fb3-a198-e4d975525d76.png"
         },
